08ebbdbad932742755c5fd202a94da68e4db59fd
Kyle White <kywhi@microsoft.com>
2022-10-03 07:58:44 -0400

    Initiaal StructuredLogViewer.Cocoa project

---
+++ src/StructuredLogViewer.Cocoa/Main.cs
@@ -0,0 +1,6 @@
+﻿using StructuredLogViewer.Cocoa;
+
+// This is the main entry point of the application.
+NSApplication.Init ();
+NSApplication.Main (args);
+
---
+++ src/StructuredLogViewer.Cocoa/ViewController.cs
@@ -0,0 +1,27 @@
+﻿using ObjCRuntime;
+
+namespace StructuredLogViewer.Cocoa;
+
+public partial class ViewController : NSViewController {
+    protected ViewController (NativeHandle handle) : base (handle)
+    {
+    }
+
+    public override void ViewDidLoad ()
+    {
+        base.ViewDidLoad ();
+
+        
+        // Do any additional setup after loading the view.
+    }
+
+    public override NSObject RepresentedObject {
+        get => base.RepresentedObject;
+        set {
+            base.RepresentedObject = value;
+
+            // Update the view, if already loaded.
+        }
+    }
+}
+
